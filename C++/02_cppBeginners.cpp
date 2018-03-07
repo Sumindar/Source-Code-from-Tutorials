37956de21d0a9382ecb6fdb7babcfcce40608a4e
Sumindar <32561380+Sumindar@users.noreply.github.com>
2018-03-07 21:03:49 +0530

    Update 02_cppBeginners.cpp

--- C++/02_cppBeginners.cpp
+++ C++/02_cppBeginners.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 
@@ -7,5 +8,6 @@ int main()
 {
 
   cout << "hello world";
+  cout<<"this world is a beautiful place to live in";
   return 0;
 }
